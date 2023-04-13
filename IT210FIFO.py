d0f4ceabec8ff9e075a9c5b2665a2cad3ae7e541
Divine <90530567+ProjVR1@users.noreply.github.com>
2023-04-13 15:24:14 -0600

    Add files via upload

---
+++ IT210FIFO.py
@@ -0,0 +1,34 @@
+print("FIFO page replacement algorithm")
+
+
+size = int(input("Enter cache size: "))
+
+
+num_pages = int(input("Enter number of pages: "))
+
+
+counter = 0
+stack = []
+
+
+for i in range(num_pages):
+    page = input("Enter page value: ")
+
+    
+    if page in stack:
+        stack.remove(page)
+    else:
+        if len(stack) == size:
+            stack.pop(0)
+        counter += 1
+    
+    stack.append(page)
+
+    
+    print(f"Page {page} inserted into cache: {stack}")
+    
+
+print("FIFO page replacement algorithm completed")
+
+
+
